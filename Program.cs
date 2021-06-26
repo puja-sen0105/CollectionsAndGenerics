66423342c9f8fc0976b8b87939f98f07bebb53e0
Sen, Puja <puja.sen@cgi.com>
2021-06-26 20:37:39 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CollectionsAndGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
